bc8d5d0468fba027f2184edf6c2317f2968c1405
Suyco <pedro.suycp@unmsm.edu.pe>
2025-06-01 14:55:13 -0500

    Search for category v1.0

---
+++ app/(client)/categoria/[level]/[id]/page.tsx
@@ -0,0 +1,74 @@
+import ProductSection from "@/app/components/products/ProductSection";
+import CategoryBox from "@/app/components/ui/CategoryBox";
+import {categorias} from "@/lib/categorias";
+
+//Funcion que trae el nombre de los id's, permite hacer esto:  "Inicio/perifericos/monitor"
+function getBreadcrumb(level: number, id: number): string[] {
+  const path: string[] = ["Inicio"];
+
+  for (const cat1 of categorias) {
+    if (level === 1 && cat1.id === id) {
+      path.push(cat1.nombre);
+      break;
+    }
+    for (const cat2 of cat1.subcategorias) {
+      if (level === 2 && cat2.id === id) {
+        path.push(cat1.nombre, cat2.nombre);
+        break;
+      }
+      for (const cat3 of cat2.subsubcategorias ?? []) {
+        if (level === 3 && cat3.id === id) {
+          path.push(cat1.nombre, cat2.nombre, cat3.nombre);
+          break;
+        }
+      }
+    }
+  }
+
+  return path;
+}
+
+export default function CategoriaPage({ params }: { params: { level: string; id: string } }) {
+  const categoryLevel = Number(params.level);
+  const categoryId = Number(params.id);
+
+  // Breadcrumb
+  const breadcrumb = getBreadcrumb(categoryLevel, categoryId);
+
+  return (
+    <div className="flex gap-4 px-4 py-6">
+      {/* Lado izquierdo - Categorías */}
+      <div className="w-64 shrink-0">
+        <CategoryBox />
+      </div>
+
+      {/* Lado derecho - Breadcrumb y productos */}
+        <div className="flex-1 flex flex-col gap-4">
+            {/* Breadcrumb */}
+            <div className="flex justify-center">
+                <nav className="text-sm text-gray-600">
+                    {breadcrumb.map((item, i) => (
+                    <span key={i}>
+                        {i > 0 && " / "}
+                        <span className={i === breadcrumb.length - 1 ? "text-red-500" : ""}>
+                        {item}
+                        </span>
+                    </span>
+                    ))}
+                </nav>
+            </div>
+        
+
+            {/* Productos */}
+            <ProductSection
+            title="Productos en esta categoría"
+            filterType="byCategory"
+            categoryLevel={categoryLevel}
+            categoryId={categoryId}
+            limit={20}
+            asCarousel={false}
+            />
+        </div>
+    </div>
+  );
+}
